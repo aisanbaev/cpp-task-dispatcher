1b43a366a2611b23444d3386ac482c00598e75d3
isanbaev <a.isanbaev@prosoftsystems.ru>
2025-12-19 13:42:58 +0000

    add: unbounded queue and tests

--- include/queue/unbounded_queue.hpp
+++ include/queue/unbounded_queue.hpp
@@ -1,18 +1,26 @@
 #pragma once
 #include "queue/queue.hpp"
+#include <condition_variable>
+#include <mutex>
+#include <queue>
 
 namespace dispatcher::queue {
 
 class UnboundedQueue : public IQueue {
-    // здесь ваш код
+private:
+    mutable std::mutex mutex_;           // Защита доступа к внутренним данным
+    std::condition_variable not_empty_;  // Условие: очередь не пуста (для pop)
+    std::queue<std::function<void()>> queue_;
+
 public:
-    explicit UnboundedQueue(int capacity);
+    // Конструктор теперь не принимает capacity
+    explicit UnboundedQueue();
 
     void push(std::function<void()> task) override;
 
     std::optional<std::function<void()>> try_pop() override;
 
-    ~UnboundedQueue() override;
+    ~UnboundedQueue() override = default;
 };
 
 }  // namespace dispatcher::queue
